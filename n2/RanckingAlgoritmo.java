ea9c330823cb078e73b3399a26b97bbe5149543f
Acassio Vilas Boas <acassio.dev@outlook.com>
2020-10-02 20:29:28 -0300

    chore: metodo insertionSort

--- n2/RanckingAlgoritmo.java
+++ n2/RanckingAlgoritmo.java
@@ -1,9 +1,72 @@
+import java.util.Random;
 
 public class RanckingAlgoritmo {
 
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        long elapsed = System.currentTimeMillis() - start;
+        int[] vetor = gerarVetor(100);
+        String str = "";
 
+        str += "\nBolha: " + bubble(vetor);
+        str += "\nInsert: " + insert(vetor);
+        str += "\nSelect: " + selection(vetor);
+
+    }
+
+    public static long bubble(int[] vetor) {
+        boolean trocou = false;
+        int aux = 0;
+        long start = System.currentTimeMillis();
+        do {
+            trocou = false;
+            for (int i = 0; i < vetor.length - 1; i++) {
+                if (vetor[i] > vetor[i + 1]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                    trocou = true;
+                }
+            }
+        } while (trocou);
+        long elapsed = System.currentTimeMillis() - start;
+        return elapsed;
+
+    }
+
+    public static long selection(int[] vetor) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < vetor.length; i++) {
+            int menor = i;
+            for (int j = i + 1; j < vetor.length; j++)
+                if (vetor[j] < vetor[menor])
+                    menor = j;
+            int aux = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = aux;
+        }
+        long elapsed = System.currentTimeMillis() - start;
+        return elapsed;
+    }
+
+    public static long insert(int[] vetor) {
+        long start = System.currentTimeMillis();
+        for (int i = 1; i < vetor.length; i++) {
+            int aux = vetor[i];
+            int j = i;
+            while (j > 0 && vetor[j - 1] >= aux) {
+                vetor[j] = vetor[j - 1];
+                j--;
+            }
+            vetor[j] = aux;
+        }
+        long elapsed = System.currentTimeMillis() - start;
+        return elapsed;
+    }
+
+    public static int[] gerarVetor(int n) {
+        int[] v = new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < v.length; i++)
+            v[i] = rand.nextInt(99) + 1;
+        return v;
     }
 }
