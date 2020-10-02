db23f277f7cfb9af9b059fde4e186265b2d4385e
Acassio Vilas Boas <acassio.dev@outlook.com>
2020-10-02 19:10:49 -0300

    chore:  metodo bolha ou bubble sort

---
+++ n2/BubbleSort.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class BubbleSort{
+
+    public static void main(String[] args){
+
+    }
+}
--- n2/SelectionSort.java
+++ n2/SelectionSort.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class selectionSort{
+public class SelectionSort{
     
     public static void main(String[] args){
         int[] vetor = gerarVetor(5);
